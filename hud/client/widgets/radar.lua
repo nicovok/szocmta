c7f5fe149d24f6757dc0f7d9c1227ec0a166ea63
nicovok <47064115+nemNico@users.noreply.github.com>
2021-05-18 22:14:00 +0200

    Blips added to radar system.

--- hud/client/widgets/radar.lua
+++ hud/client/widgets/radar.lua
@@ -1,4 +1,5 @@
 local renderTarget = dxCreateRenderTarget(1, 1)
+local blipTarget = dxCreateRenderTarget(3000, 3000, true)
 
 local map = loadTexture('client/images/map.png')
 local mapW, mapH = dxGetMaterialSize(map)
@@ -26,13 +27,25 @@ renders.radar = function(self)
 
     local _, _, camRot = getElementRotation(getCamera())
 
-    local elementX, elementY = targetW/2 -playerX/div, targetH/2 + playerY/div
+    local elementX, elementY = targetW/2 - playerX/div, targetH/2 + playerY/div
     local startX, startY = elementX - mapW/2, elementY - mapH/2
+
+    dxSetRenderTarget(blipTarget, true)
+
+        for blip, data in pairs(blipCache) do
+            local blipX, blipY = getElementPosition(blip)
+
+            local size = 30
+            local x, y = 1500 + blipX/2, 1500 - blipY/2
+
+            dxDrawImage(x - size/2, y - size/2, size, size, 'client/images/blips/' .. data.icon .. '.png', -camRot)
+        end
 
     dxSetRenderTarget(renderTarget, true)
 
         dxDrawRectangle(0, 0, targetW, targetH, waterColor)
         dxDrawImage(startX, startY, mapW, mapH, map, camRot, (playerX/div), -(playerY/div), tocolor(255, 255, 255, 255))
+        dxDrawImage(startX, startY, mapW, mapH, blipTarget, camRot, (playerX/div), -(playerY/div), tocolor(255, 255, 255, 255))
 
     dxSetRenderTarget()
 
@@ -71,3 +84,51 @@ renders.radar = function(self)
 
     dxDrawImage(x, y, size, size, 'client/images/arrow.png', -playerRot + cameraRot)
 end
+
+--> Blips
+blipCache = {}
+
+addEventHandler('onClientResourceStart', resourceRoot,
+    function()
+        local gs = createBlip(-2445.0876464844, 974.73699951172, 50.3046875)---2445.0876464844, 974.73699951172, 50.3046875
+        setElementData(gs, 'blip.icon', 'gasstation')
+
+        local hosp = createBlip(-2659.38671875, 632.03253173828, 14.453125)---2445.0876464844, 974.73699951172, 50.3046875
+        setElementData(hosp, 'blip.icon', 'hospital')
+
+        local blips = getElementsByType('blip')
+        for i, blip in pairs(blips) do
+            blipCache[blip] = {
+                important = getElementData(blip, 'blip.important'),
+                icon = getElementData(blip, 'blip.icon') or 'default',
+            }
+        end
+    end
+)
+
+addEventHandler('onClientElementDataChange', root,
+    function(key, _, value)
+        if getElementType(source) == 'blip' then
+            if not blipCache[source] then
+                return
+            end
+
+            if key == 'blip.important' then
+                blipCache[source].important = value
+            elseif key == 'blip.icon' then
+                blipCache[source].icon = value or 'default'
+            end
+        end
+    end
+)
+
+addEventHandler('onClientElementStreamIn', root,
+    function(key, _, value)
+        if getElementType(source) == 'blip' then
+            blipCache[source] = {
+                important = getElementData(source, 'blip.important'),
+                icon = getElementData(source, 'blip.icon') or 'default',
+            }
+        end
+    end
+)
