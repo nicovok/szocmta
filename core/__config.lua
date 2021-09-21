461fe439327edfaa0a2bc78fc382be6c74606780
nicovok <47064115+nicovok@users.noreply.github.com>
2021-09-21 18:14:31 +0200

    Update __config.lua

--- core/__config.lua
+++ core/__config.lua
@@ -7,8 +7,7 @@ MAP_NAME     = 'Budapest'
 GAME_TYPE    = 'Roleplay'
 
 DEVELOPERS = {
-    ['CC0FD9D5065233298CD92820B7B68D13'] = 'Nico',
---  ['4EC5D0EE6795AE7302B3FD589E012543'] = 'Bence58',
+    [''] = 'Nico',
 }
 
 SERVER_TAGS = {
@@ -25,8 +24,8 @@ ID_COMMAND = 'id'
 ID_CONTENT = 'A csatlakozási azonosítód: con_id' -- %con_id%
 
 DATABASE = {
-    host     = '79.139.57.127',
-    dbname   = 's7_szocmta',
-    username = 'u7_FHs3lP0NP9',
-    password = 'z@JjlgW.KEjudXlx8wMX5vV9',
+    host     = '',
+    dbname   = '',
+    username = '',
+    password = '',
 }
